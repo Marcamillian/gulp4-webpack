8984519b36889a5f5bca87d5ece94462029d8e66
Marcamillian <marcamillian@gmail.com>
2019-04-17 16:14:52 +0100

    feat: working with react
    package.json
    - install react & react-dom packages
    
    main.js
    - add target to the ReactDOM call

--- react-site/js/main.js
+++ react-site/js/main.js
@@ -1,15 +1,10 @@
-/*
-import React from 'node_modules/react';
-import ReactDOM from 'node_modules/react-dom';
 
-import TitleBar from './components/title-bar';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TitleBar from '../components/title-bar';
 
 ReactDOM.render(
   <TitleBar />
+  ,document.querySelector('.container')
 )
-*/
-
-import testFunction from '../components/test-function';
-
-console.log("running main")
-testFunction()
